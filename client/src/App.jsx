5ee7d8e3e0a0db8124b03ce7ba1638c72512ecd6
anuj1101 <kranuj952@gmail.com>
2025-12-23 14:50:41 +0530

    link updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-r0vt.onrender.com");
 
 function App() {
   const nameRef = useRef(null);
